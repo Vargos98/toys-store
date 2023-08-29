f228d67a7397bf65382172a32d993a3bb3aef00b
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-29 08:23:51 +0530

    Added react-icons and tailwind css dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
+
 const Navbar = () => {
   return (
-    <div className='h-50px w-full bg-black'>
+    <div >
         <div>
-            <h1 className='text-yellow fs-5'>Toys or Us!</h1>
+            <h1 >Toys or Us!</h1>
         </div>
         <div>
             <ul >
-                <li >Home</li>
+                <li>Home</li>
                 <li>About</li>
                 <li>Products</li>
                 <li>Location</li>
