e15cf12594f56fdf2b41ae6d3a5106b5f4c139c7
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-22 20:02:02 +0530

    Created all of the components including there respective css files, also maintained file structure for better readability.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import About from './components/About/About'
+import Navbar from './components/Navbar/Navbar'
+import Header from './components/Header/Header'
+import Location from './components/Location/Location'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    <Navbar/>
+    <Header/>
+    <About/>
+    <Location/>
+    <Contact/>
+    </div>
   )
 }
 
---
+++ src/components/Location/Location.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./location.css"
+const Location = () => {
+  return (
+    <div>
+      <h1>Location</h1>
+    </div>
+  )
+}
+
+export default Location
