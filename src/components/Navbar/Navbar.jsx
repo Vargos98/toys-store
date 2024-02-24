cfdb308c3a13493e2c7bfb7f89f8e82b8b27371b
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-24 18:28:31 +0530

    Worked on Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./navbar.css"
+import logo from "../../assets/logo.png"
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className='navbar-container'>
+        {/* <img src={logo} alt="logo" className='nav-logo' /> */}
+        <a href="#" className='head'>Krishna Toys</a>
+        <ul className='nav-items'>
+          <li className='nav-li'>About</li>
+          <li className='nav-li'>Location</li>
+          <li className='nav-li'>Contact</li>
+        </ul>
+    
     </div>
   )
 }
