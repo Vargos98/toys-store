e703bf90dee3e40a37c82b03426266fd72422793
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-24 19:55:31 +0530

    Update Navbar.jsx
    Added close and open functionality on the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
+import {useRef} from "react"
 import { FaBars, FaTimes } from "react-icons/fa"
 import "./navbar.css"
 // import logo from "../../assets/logo.png"
 const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = ()=>{
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
   return (
     <nav className='navbar-container'>
       {/* <img src={logo} alt="logo" className='nav-logo' /> */}
-      <a href="/" className='head'>Krishna Toys</a>
-      <div className='nav-items'>
-        <a href='/' className='nav-li'>About</a>
-        <a href='/' className='nav-li'>Location</a>
-        <a href='/' className='nav-li'>Contact</a>
-        <button>
-          <FaTimes />
-        </button>
-      </div>
-      <button>
-        <FaBars />
-      </button>
+          <a href="/" className='head'>Krishna Toys</a>
+                <div className='nav-items'>
+                  <a href='/' className='nav-li'>About</a>
+                  <a href='/' className='nav-li'>Location</a>
+                  <a href='/' className='nav-li'>Contact</a>
+                  <button className="nav-btn nav-close-btn"onClick={showNavbar}>
+                      <FaTimes />
+                  </button>
+                </div>
+          <button className='nav-btn' onClick={showNavbar}>
+              <FaBars  />
+          </button>
 
     </nav>
   )
