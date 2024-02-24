f009511e46721692b141958c85c1fdcb89a1ff40
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-24 20:54:50 +0530

    Created header.jsx
    will complete header by tomorrow

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import "./header.css"
+import header from "../../assets/header.png"
 const Header = () => {
   return (
-    <div>
-    <h1>Header</h1>
+    <div className='header'>
+      <div className="left">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, recusandae.<br>
+
+        </br> Excepturi ipsum minima unde assumenda fuga iure, quasi recusandae placeat culpa quam facere sapiente odit quis?</p>
+      </div>
+      <div className="right">
+        <img src={header} alt="" />
+      </div>
     </div>
   )
 }
