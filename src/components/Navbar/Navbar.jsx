be3c1824c1100c65699e8ce43f976e7e30d2943e
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-24 19:49:34 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
-import {FaBars, FaTimes} from "react-icons/fa"
+import { FaBars, FaTimes } from "react-icons/fa"
 import "./navbar.css"
 // import logo from "../../assets/logo.png"
 const Navbar = () => {
   return (
     <nav className='navbar-container'>
-        {/* <img src={logo} alt="logo" className='nav-logo' /> */}
-        <a href="/" className='head'>Krishna Toys</a>
-        <div className='nav-items'>
-          <a href='/' className='nav-li'>About</a>
-          <a href='/' className='nav-li'>Location</a>
-          <a href='/' className='nav-li'>Contact</a>
-          <button>
-            <FaBars/>
-          </button>
-        </div>
+      {/* <img src={logo} alt="logo" className='nav-logo' /> */}
+      <a href="/" className='head'>Krishna Toys</a>
+      <div className='nav-items'>
+        <a href='/' className='nav-li'>About</a>
+        <a href='/' className='nav-li'>Location</a>
+        <a href='/' className='nav-li'>Contact</a>
         <button>
-          <FaTimes/>
+          <FaTimes />
         </button>
-    
+      </div>
+      <button>
+        <FaBars />
+      </button>
+
     </nav>
   )
 }
