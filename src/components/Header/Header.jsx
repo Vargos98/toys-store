0316bdb5053aab22d54a7a8c617f70171f662c94
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-25 14:01:31 +0530

    Finalized header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import "./header.css"
 import background from "../../assets/background.png"
-import tria from "../../assets/2.jpg"
+
 const Header = () => {
   return (
     <div className='header' id='home'>
       {/* <div className="shade"></div> */}
-    
-        <div className="left">
-          <p className='header-para'>The Toys <br/>you can <br />Trust On!</p>
-        </div>
-        <div className="right">
-          <img src={background} alt="background" className='header-img' />
-        </div>
 
- 
+      <div className="left">
+        <p className='header-para'>
+          The Toys <br />you can
+          <br />Trust
+          <span className='high'> On!</span>
+        </p>
+      </div>
+      <div className="right">
+        <img src={background} alt="background" className='header-img' />
+      </div>
+
+
 
 
     </div>
