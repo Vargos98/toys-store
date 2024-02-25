46c995cd5e712bd379c75b61ff2be72fed670faa
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-02-25 11:26:29 +0530

    Started to work on Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import "./header.css"
 import header from "../../assets/header.png"
+import tria from "../../assets/2.jpg"
 const Header = () => {
   return (
     <div className='header'>
-      <div className="left">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, recusandae.<br>
-
-        </br> Excepturi ipsum minima unde assumenda fuga iure, quasi recusandae placeat culpa quam facere sapiente odit quis?</p>
-      </div>
-      <div className="right">
-        <img src={header} alt="" />
-      </div>
+    <div className="shade"></div>
+      <p className='header-para'>The Toys <br/>you can Trust On!</p>
+      
     </div>
   )
 }
